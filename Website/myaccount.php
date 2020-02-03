2c197f4e71e05036c664688ca38fa5e48f396c77
jagjotsingh1 <35777634+jagjotsingh1@users.noreply.github.com>
2020-02-02 23:32:41 -0500

    Demo push - Mitchell
    Credit to Mitchell for demo integration with website/pushing files

--- Website/myaccount.php
+++ Website/myaccount.php
@@ -18,10 +18,10 @@
             <ul class="navbar-nav mr-auto">
                <li class="nav-item"><a class="nav-link" href="http://34.66.171.142/index.php">Home</a></li>
                <li class="nav-item"><a class="nav-link" href="http://34.66.171.142/about.html">About</a></li>
-               <li class="nav-item"><a class="nav-link" href="http://34.66.171.142/download.html">Download</a></li>
                <li class="nav-item"><a class="nav-link" href="https://github.com/JTBridges/Capstone-Defence.git">GitHub</a></li>
                <li class="nav-item"><a class="nav-link" href="http://34.66.171.142/login.php">Login</a></li>
                <li class="nav-item"><a class="nav-link" href="http://34.66.171.142/register.php">Register</a></li>
+               <li class="nav-item"><a class="nav-link" href="http://34.66.171.142/UnityIndex.html">Demo</a></li>
             </ul>
          </div>
       </nav>
