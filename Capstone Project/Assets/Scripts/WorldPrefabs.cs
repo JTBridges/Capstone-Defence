a02904f631f611414092b5224bc1f14b8f00e5b7
James Bridges <jbridges@oakland.edu>
2020-02-08 23:52:52 -0500

    Update 8
    Removed some debugs and added starting prefabs for better world generation

---
+++ Capstone Project/Assets/Scripts/WorldPrefabs.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldPrefabs : MonoBehaviour
+{
+    public GameObject Barrel;
+    public GameObject Wall1;
+    public GameObject Wall2;
+    public GameObject Bridge;
+    public GameObject BridgeWide;
+    public GameObject woodBox;
+    public GameObject grass1;
+    public GameObject grass2;
+    public GameObject ground1;
+    public GameObject ground2;
+    public GameObject spawnGround;
+    public GameObject appleTreeShort;
+    public GameObject appleTreeTall;
+    public GameObject roundTreeShort;
+    public GameObject roundTreeTall;
+    public GameObject pineTreeShort;
+    public GameObject pineTreeShort2;
+    public GameObject pineTreeTall;
+    public GameObject pineTreeTall2;
+
+    List<int> list1 = new List<int>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
