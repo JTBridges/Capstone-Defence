06d496c81542b88fadaff3be941a1d6f31c32ef5
Mitchell OHair <mlohair@oakland.edu>
2020-02-11 21:13:13 -0500

    Menu
    Landing should be applications starting scene now. Play button loads "Start" scene, and plays a sound. Quit button should close application.(not in editor though). Gear icon in "Start" Scene will reload the menu. I want to have a resume button that brings the player right back to where they left off if the came to the menu from a game, however it seems that there is no canned function to save the entirety of a scene in Unity? I am looking into serialization/persistent memory/easy save asset/ etc.

---
+++ Capstone Project/Assets/Scripts/RightupSetScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RightupSetScript : MonoBehaviour
+{
+    public void menu()
+    {
+        SceneManager.LoadScene("Landing");
+    }
+}
