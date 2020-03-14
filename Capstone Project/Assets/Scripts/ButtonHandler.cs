7c6e18fa199aeb7249b2931803d4efc77dcd41e4
Mitchell OHair <mlohair@oakland.edu>
2020-03-14 15:36:37 -0400

    Landing Screen and Settings
    Not done yet, just want to resync my repo

---
+++ Capstone Project/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public GameObject SettingsPopup;
+
+    public void NewGame()
+    {
+        NewOrLoad.Load = false;
+        SceneManager.LoadScene("Start");
+    }
+    public void Settings()
+    {
+        SettingsPopup.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+        SettingsPopup.transform.localScale = new Vector3(0.46f, 0.4f, 1);
+        SettingsPopup.transform.position = new Vector2(960, 530);
+    }
+    public void CloseSettings()
+    {
+        SettingsPopup.transform.SetParent(null);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
